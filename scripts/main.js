fe628b020b08dd6325362dd62fbf607444980397
zyang99 <1402687676@qq.com>
2021-03-11 16:21:55 +0800

    x

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,28 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
 
-function multiply(num1,num2){
-    let result = num1 * num2;
-    return result;
-}
+// function multiply(num1,num2){
+//     let result = num1 * num2;
+//     return result;
+// }
 
-//点击图片切换图片
+/**
+ * -----------点击图片切换图片
+ */
 let myImage = document.querySelector('img');
 myImage.onclick = function(){
     //获取img的属性值src
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/joker.png'){
-        myImage.setAttribute('src','images/tom.jpg');
+        myImage.setAttribute('src','images/Tom.jpg');
     }else{
         myImage.setAttribute('src','images/joker.png');
     }
 }
 
-//进入用户名欢迎（输入校准）
+/**
+ * ------进入用户名欢迎（输入校准）
+ */
 let myButton = document.querySelector('button');
 function setUsername(){
     let myName = prompt('输入名字');
@@ -39,16 +43,21 @@ myButton.onclick = function(){
     setUsername();
 }
 
-//跳转猜数字游戏界面
+/**
+ * -------跳转猜数字游戏界面
+ */
 let guessButton = document.querySelector(".guess-number");
-guessButton.onclick = function(){
+//直接用匿名函数
+// guessButton.onclick = function(){
+//     location.href= "guess-number.html";
+// }
+//还没有点击就会跳转   函数名字后面的()是函数调用运算符
+//guessButton.onclick = jump();
+//只有点击按钮才会跳转
+//guessButton.onclick = jump;
+function jump(){
     location.href= "guess-number.html";
 }
-
-// let myHtml = document.querySelector('html');
-// myHtml.onclick = function(){
-//     alert('疼，轻一点！');
-// }
-// document.querySelector('html').onclick = function(){
-//          alert('疼，轻一点！');
-// }
+//另外一种方式
+//guessButton.addEventListener('click',jump);
+//#########所以没有参数的函数调用上面都可以选择，但是有参数调用的函数使用匿名函数的方式来进行调用
